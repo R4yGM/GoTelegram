5bb987d6c7de4a1c683ece5b6a92483b91de659f
R4yan <45489155+R4yGM@users.noreply.github.com>
2020-01-09 21:12:32 +0100

    Update GoTelegram.go

--- GoTelegram.go
+++ GoTelegram.go
@@ -1,1 +1,23 @@
+package GoTelegram
 
+import(
+        "time"
+        //log "./golang/src/github.com/r4y/GoTelegram/log"
+        logging "github.com/r4y/GoTelegram/logging"
+        "fmt"
+)
+type Token struct {
+
+     Value string
+
+}
+
+func (t Token) BuildBot() {
+ logger := logging.New(time.RFC3339, true)
+ logger.Log("info", "starting up service")
+ return fmt.Sprintf("here is the token: %d ", t.Value)
+}
+
+func Hello() {
+    fmt.Println("Hello, World!")
+}
